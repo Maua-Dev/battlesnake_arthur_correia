a3f5c695d83c502c8f2e41010c8c54720dbbad88
Arthur199S <arthursilvacorreia199@gmail.com>
2025-10-08 01:17:18 -0300

    The Final Boss

--- src/main/java/com/mauadev/code/Handler.java
+++ src/main/java/com/mauadev/code/Handler.java
@@ -181,12 +181,6 @@ public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIG
                     return false;
                 }
             }
-            Coordinate headInimigo = s.getHead();
-            int dx = Math.abs(nx - headInimigo.getX());
-            int dy = Math.abs(ny - headInimigo.getY());
-            if (dx <= 1 && dy <= 1){
-                return false;
-            }
         }
         }
         return true;
