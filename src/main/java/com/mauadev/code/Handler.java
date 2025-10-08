2dc6b822dfa149bc70ff75f328e728709d40a417
Arthur199S <arthursilvacorreia199@gmail.com>
2025-10-08 09:23:46 -0300

    THE BOSS

--- src/main/java/com/mauadev/code/Handler.java
+++ src/main/java/com/mauadev/code/Handler.java
@@ -183,7 +183,7 @@ public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIG
                 Coordinate headInimigo = s.getHead();
                 int dx = Math.abs(nx - headInimigo.getX());
                 int dy = Math.abs(ny - headInimigo.getY());
-                if (dx <= 1 && dy <= 1){
+                if (dx <= 1 || dy <= 1){
                     return false;
                 }
             }
